ad67a0ab69cc79735fdf7f504919694d191d134f
robherron <rlherron100@gmail.com>
2021-02-06 17:57:31 -0500

    General Clean Up

--- src/components/Modal.js
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import React from "react";
-import Photo from "./Photo";
-export default class Modal extends Component {
-
-    render
-    return(
-    (<Photo/>);)}
