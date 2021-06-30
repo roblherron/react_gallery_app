a98d14087860513d4558c80a67346e147874e7f4
robherron <rlherron100@gmail.com>
2021-06-30 14:24:58 -0400

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-/*
- * High level principles for app:
- * (1) Single Source of Truth: for all values and data
- * (2) Separation of Concerns: only pass data as necessary
- */
-
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
@@ -12,17 +6,6 @@ import PhotoContainer from "./components/PhotoContainer";
 import Search from "./components/Search";
 import Nav from "./components/Nav";
 
-/**
- * Notes on Application state:
- * (1) this.state.query (what does this do? A: populates search bar; what is it named? A: searchValue; is it necessary at all? A: yes)
- * (2) the url value ?q=xyz (query param; defined as queryState)
- * The source of truth is (2)
- * We are both reading and writing (1)
- * We are only writing (2)
- * TODOs:
- * (A) We are both reading and writing (2)
- * (B) (1) is subordinate to (2)
- **/
 export default class App extends Component {
   render() {
     return (
